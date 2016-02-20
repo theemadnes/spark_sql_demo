ab580552079c4e86128a203809a2ccec4ab6d3b0
theemadnes <alex.mattson@gmail.com>
2016-02-20 01:45:34 -0800

    query written

--- pyspark_sql_code/spark_sql_processing.py
+++ pyspark_sql_code/spark_sql_processing.py
@@ -17,6 +17,14 @@ sql_context = HiveContext(sc) # create a Hive context
 # load data file from s3
 print('Loading file from S3...')
 source_file = sc.textFile('s3://mattsona-public/spark_demo_data/shirt_orders.csv')
+
 # split each line on ',' as this is a csv
 lines = source_file.map(lambda x: x.split(','))
 lines_inferred_types_rows = lines.map(lambda x: Row(full_name = x[0], shirt_size  = x[1], us_state = x[2], shirt_quantity = int(x[3])))
+
+# turn it in to a DataFrame
+lines_schema = sqlContext.createDataFrame(lines_inferred_types_rows)
+lines_schema.registerTempTable("t_shirt_orders")
+
+# execute SQL statement over DataFrame for quantity >= 100
+orders_over_100 = sqlContext.sql("SELECT * FROM t_shirt_orders WHERE shirt_quantity >= 100")

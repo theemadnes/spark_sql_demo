415890da94f70ad9c56c87b78222bd6583546d6b
theemadnes <alex.mattson@gmail.com>
2016-02-19 17:47:08 -0800

    created sql processing file

--- pyspark_sql_code/spark_sql_processing.py
+++ pyspark_sql_code/spark_sql_processing.py
@@ -1,1 +1,11 @@
 # simple way to launch this is '/usr/bin/spark-submit ./spark_sql_processing.py'
+
+# import critical modules
+from __future__ import print_function # support python 2.7 & 3
+from pyspark import SparkContext
+import datetime
+
+# define params
+s3_target_bucket_name = 'mattsona-public'
+
+sc = SparkContext() # create a spark context

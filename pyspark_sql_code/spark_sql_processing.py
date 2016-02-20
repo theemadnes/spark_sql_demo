ab75f8e8a1b52675622895977470a2aa5f6cd5d1
theemadnes <alex.mattson@gmail.com>
2016-02-19 18:03:09 -0800

    incremental updates to python code

--- pyspark_sql_code/spark_sql_processing.py
+++ pyspark_sql_code/spark_sql_processing.py
@@ -3,9 +3,17 @@
 # import critical modules
 from __future__ import print_function # support python 2.7 & 3
 from pyspark import SparkContext
+from pyspark.sql import HiveContext
+from pyspark.sql.types import *
 import datetime
 
 # define params
 s3_target_bucket_name = 'mattsona-public'
 
 sc = SparkContext() # create a spark context
+sql_context = HiveContext(sc) # create a Hive context
+
+# create an RDD from the source file first
+# load data file from s3
+print('Loading file from S3...')
+source_file = sc.textFile('s3://mattsona-public/spark_demo_data/shirt_orders_header.csv')
